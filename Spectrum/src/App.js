ea0dcd8de2a809a94a2a7810094766bf8eef5af3
Vishal Maurya <vishalmaurya3112@gmail.com>
2022-04-27 11:09:02 +0530

    navbar

---
+++ Spectrum/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Index from './components/Navbar/Index';
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+      <div className='bg-indigo-600 w-full h-screen'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
